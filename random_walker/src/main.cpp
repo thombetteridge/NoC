2d610884ada5fcfdbb471267b76df63217ec2c66
tomB <thombetteridge@gmail.com>
2024-10-24 22:51:51 +1030

    random walker

--- random_walker/src/main.cpp
+++ random_walker/src/main.cpp
@@ -1,4 +1,40 @@
+#include <math.h>
 #include <raylib.h>
+#include <raymath.h>
+#include <vector>
+
+class Walker
+{
+public:
+  Walker()
+
+  {
+    pos.x = GetRandomValue(0, GetScreenWidth());
+    pos.y = GetRandomValue(0, GetScreenHeight());
+  }
+
+  void update()
+  {
+    path.push_back(pos);
+    const float x = GetRandomValue(-100, 100) / 100.0f;
+    const float y = GetRandomValue(-100, 100) / 100.0f;
+
+    const auto move = Vector2{x, y};
+
+    pos = Vector2Add(pos, move);
+  }
+
+  void draw()
+  {
+    DrawLineStrip(path.data(), path.size(), BLUE);
+    DrawCircle(pos.x, pos.y, 3, RED);
+  }
+
+private:
+  Vector2              pos;
+  std::vector<Vector2> path;
+};
+
 
 int main(void)
 {
@@ -10,13 +46,26 @@ int main(void)
 
   InitWindow(screenWidth, screenHeight, "raylib [core] example");
 
+  std::vector<Walker> walkers;
+
+  for (int i = 0; i < 100; ++i) {
+    Walker w;
+    walkers.push_back(w);
+  }
+
   // Main game loop
   while (!WindowShouldClose()) {
     // Update
+    for (auto& w: walkers) {
+      w.update();
+    }
     // Draw
     {
       BeginDrawing();
       ClearBackground(RAYWHITE);
+      for (auto& w: walkers) {
+        w.draw();
+      }
       EndDrawing();
     }
   }
